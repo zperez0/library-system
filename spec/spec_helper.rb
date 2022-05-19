4c5103d50f7e9905fd4b5a79fbf90fdb3218ea12
Yazmin Perez <zoeperez333@gmail.com>
2022-05-18 20:52:13 -0700

    add spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,18 @@
-source('https://rubygems.org')
+require 'rspec'
+require 'pg'
+require 'book'
+require 'patron'
+require 'checkout'
+require 'pry'
 
-gem('sinatra')
-gem('sinatra-contrib')
-gem('rspec')
-gem('capybara')
-gem('pry')
-gem('pg')
+# shared code for clearing tests between runs & connecting to the DB:
+
+DB = PG.connect({:dbname => 'library_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM books *;")
+    DB.exec("DELETE FROM patrons *;")
+    DB.exec("DELETE FROM checkouts *;")
+  end
+end
